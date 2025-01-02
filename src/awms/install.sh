666c5e6c6fad0b9e487310ade9ce8efd611c71a1
Shaun Turner <shaun.turner1@nhs.net>
2025-01-02 14:28:29 +0000

    split out awms from jupyterhub

--- src/awms/install.sh
+++ src/awms/install.sh
@@ -4,24 +4,20 @@
 
 cp -R ./modules/* "${KRAPCTL_MODULES}"
 
-mkdir -p /usr/lib/jupyterhub/etc
-mkdir -p /usr/lib/jupyterhub/work
-chmod 0777 /usr/lib/jupyterhub/work
+mkdir -p /usr/lib/awms/etc
+mkdir -p /usr/lib/awms/work
+chmod 0777 /usr/lib/awms/work
 
-cat <<EOF > /usr/lib/jupyterhub/etc/environment
-HELM_REPOSITORY="${HELMREPOSITORY}"
+cat <<EOF > /usr/lib/awms/etc/environment
 NAMESPACE="${NAMESPACE}"
-CHART_NAME="${CHARTNAME}"
-CHART_VERSION="${CHARTVERSION}"
-IMAGE_NAME="${IMAGENAME}"
-IMAGE_TAG="${IMAGETAG}"
-HUB_DOCKER_IMAGE_DEFINITION="${HUBDOCKERIMAGEDEFINITION}"
+AWMS_DOCKER_IMAGE_DEFINITION="${AWMSDOCKERIMAGEDEFINITION}"
+ANALYTICSWORKSPACE_HELM_CHART="${ANALYTICSWORKSPACEHELMCHART}"
 KUSTOMIZE_PATH="${KUSTOMIZEPATH}"
 EOF
 
 pip install kubernetes_asyncio
 pip install lscsde_workspace_mgmt
-pip install jupyterhub-kubespawner
+pip install awms-kubespawner
 pip install pytest
 pip install pytest-asyncio
 pip install build
