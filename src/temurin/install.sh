f0336958504a073afbb5350318f020ab8b9ce3c8
Shaun Turner <shaun.turner1@nhs.net>
2025-01-03 10:35:29 +0000

    fix to temurin to include JAVA_HOME

--- src/temurin/install.sh
+++ src/temurin/install.sh
@@ -3,3 +3,5 @@ wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --
 echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
 apt-get update
 apt-get install -y temurin-${TEMURINVERSION}-jdk
+apt-get install -y maven
+echo "export JAVA_HOME="/usr/lib/jvm/temurin-${TEMURINVERSION}-jdk-amd64"" >> ~/.bashrc
